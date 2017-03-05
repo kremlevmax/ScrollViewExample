33658b6aea40f2c3dc545bfefbabad18f54ec742
kremlevmax <kremlevmax1989@gmail.com>
2017-03-05 16:22:26 +0200

    - Added horizontal UIScrollview

--- ScrollviewExample/ViewController.swift
+++ ScrollviewExample/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Outlets:
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    //Variables:
+    var images = [UIImageView]()
+    
+    //Constants:
+    
+    //Actions:
+    
+    var contentWidth : CGFloat = 0.0
+    
+    
 
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x)")
+            let imageView = UIImageView(image: image)
+            
+            let coordinateX = scrollView.frame.midX + scrollView.frame.size.width*CGFloat(x) - 75
+            let coordinateY = scrollView.frame.size.height/2 - 75
+            
+            imageView.frame = CGRect(x: coordinateX, y: coordinateY, width: 150, height: 150)
+            
+            scrollView.addSubview(imageView)
+            contentWidth += scrollView.frame.size.width
+            
+        }
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: 300.0)
+        
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
